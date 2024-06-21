b969caba7e2ea29375658a3b404377eecdb6ec42
konstantinloginov <acro221996@yandex.ru>
2024-06-21 22:22:45 +0500

    Init commit

---
+++ src/app.service.ts
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import { parse } from 'node-html-parser';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { ConfigService } from '@nestjs/config';
+import { URLS } from './app.constants';
+
+@Injectable()
+export class AppService {
+  private readonly logger = new Logger(AppService.name);
+  private links = new Set();
+
+  constructor(private readonly config: ConfigService) {}
+
+  @Cron('* */30 * * * *')
+  async handleCron() {
+    const page = await this.getMemDayPage();
+    const mem = this.getRandomMemFromPage(page);
+    await this.sendMessageToTelegram(encodeURIComponent(mem));
+  }
+
+  getRandomMemFromPage(page: string): string {
+    const root = parse(page);
+
+    const images = root
+      .querySelectorAll('img')
+      .filter((img) => img.rawAttrs.toLowerCase().includes('мем'))
+      .map((img) => img.attributes.src);
+
+    const random = images[Math.floor(Math.random() * images.length)];
+
+    if (this.links.has(random)) {
+      return this.getRandomMemFromPage(page);
+    }
+
+    this.links.add(random);
+
+    return random;
+  }
+
+  async getMemDayPage(): Promise<string> {
+    const url = URLS.AnekdotRuMemDay;
+    try {
+      const result = await axios.get(url);
+      return result.data;
+    } catch (e) {
+      this.logger.error(e);
+    }
+  }
+
+  private async sendMessageToTelegram(message: string): Promise<void> {
+    const baseUrl = this.config.get('TELEGRAM_URL');
+    const token = this.config.get('TELEGRAM_TOKEN');
+    const chatId = this.config.get('TELEGRAM_CHAT_ID');
+
+    const url = `${baseUrl}/bot${token}/sendMessage?parse_mode=html&chat_id=${chatId}&text=${message}`;
+
+    await axios.get(url).catch((e) => {
+      this.logger.error(e);
+    });
+  }
+}
